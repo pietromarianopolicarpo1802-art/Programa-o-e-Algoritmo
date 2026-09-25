4719f219778470c217b43df3a65903232d798388
Pietro Mariano Policarpo <pietromarianopolicarpo1802@gmail.com>
2026-09-25 11:40:17 -0300

    Add files via upload

---
+++ 3-Bimestre/exercicio 1.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um numero inteiro: "))
+
+if numero > 0:
+    print("O numero e positivo.")
+elif numero < 0:
+    print("O numero e negativo.")
+else:
+    print("O número é zero")
